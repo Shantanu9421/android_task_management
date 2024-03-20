45c1acfae9e75f76f718976bf437a2bc8427515c
Shantanu9421 <110804946+Shantanu9421@users.noreply.github.com>
2024-03-20 07:26:51 +0530

    Add files via upload

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Prevalent/Prevalent.java
@@ -0,0 +1,13 @@
+package com.example.myapplication.Prevalent;
+
+
+
+import com.example.myapplication.Model.User;
+
+public class Prevalent {
+
+
+    public static User CurrentonlineUser;
+    public static final String UserPhoneID="UserPhone";
+    public static final String UserPasswordId="UserPassword";
+}
